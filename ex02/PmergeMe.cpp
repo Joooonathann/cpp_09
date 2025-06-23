8da8fbce01eeea44bf23dfd886fd1aa7f1c2fee3
Jonathan <peliium.2022@gmail.com>
2025-06-23 13:33:22 +0200

    ok

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -9,19 +9,23 @@
 #include <iomanip>
 #include <climits>
 
-PmergeMe::PmergeMe(void) { }
+PmergeMe::PmergeMe(void) {}
 PmergeMe::PmergeMe(const PmergeMe &copy) { *this = copy; }
-PmergeMe &PmergeMe::operator=(const PmergeMe &copy) {
-    if (this != &copy) {
+PmergeMe &PmergeMe::operator=(const PmergeMe &copy)
+{
+    if (this != &copy)
+    {
         _vector = copy._vector;
         _deque = copy._deque;
     }
     return *this;
 }
-PmergeMe::~PmergeMe(void) { }
+PmergeMe::~PmergeMe(void) {}
 
-void PmergeMe::parseInput(char **argv) {
-    for (int i = 1; argv[i]; ++i) {
+void PmergeMe::parseInput(char **argv)
+{
+    for (int i = 1; argv[i]; ++i)
+    {
         std::string arg(argv[i]);
         for (size_t j = 0; j < arg.length(); ++j)
             if (!isdigit(arg[j]))
@@ -36,33 +40,40 @@ void PmergeMe::parseInput(char **argv) {
         throw std::runtime_error("Error");
 }
 
-void PmergeMe::printSequence(const std::string &label, const std::vector<int> &sequence) const {
+void PmergeMe::printSequence(const std::string &label, const std::vector<int> &sequence) const
+{
     std::cout << label;
     for (size_t i = 0; i < sequence.size(); ++i)
         std::cout << " " << sequence[i];
     std::cout << std::endl;
 }
 
-void PmergeMe::printSequence(const std::string &label, const std::deque<int> &sequence) const {
+void PmergeMe::printSequence(const std::string &label, const std::deque<int> &sequence) const
+{
     std::cout << label;
     for (size_t i = 0; i < sequence.size(); ++i)
         std::cout << " " << sequence[i];
     std::cout << std::endl;
 }
 
-std::vector<size_t> PmergeMe::generateJacobsthalOrder(size_t n) {
+std::vector<size_t> PmergeMe::generateJacobsthalOrder(size_t n)
+{
     std::vector<size_t> order;
     size_t j1 = 1, j2 = 1;
-    while (j1 < n) {
+    while (j1 < n)
+    {
         order.push_back(j1);
         size_t tmp = j1;
         j1 = j1 + 2 * j2;
         j2 = tmp;
     }
-    for (size_t i = 0; i < n; ++i) {
+    for (size_t i = 0; i < n; ++i)
+    {
         bool found = false;
-        for (size_t j = 0; j < order.size(); ++j) {
-            if (order[j] == i) {
+        for (size_t j = 0; j < order.size(); ++j)
+        {
+            if (order[j] == i)
+            {
                 found = true;
                 break;
             }
@@ -73,14 +84,18 @@ std::vector<size_t> PmergeMe::generateJacobsthalOrder(size_t n) {
     return order;
 }
 
-void PmergeMe::fordJohnsonSort(std::vector<int> &seq) {
+void PmergeMe::fordJohnsonSort(std::vector<int> &seq)
+{
     size_t n = seq.size();
-    if (n <= 1) return;
+    if (n <= 1)
+        return;
 
     std::vector<int> bigs, smalls;
-    for (size_t i = 0; i + 1 < n; i += 2) {
-        int a = seq[i], b = seq[i+1];
-        if (a < b) std::swap(a, b);
+    for (size_t i = 0; i + 1 < n; i += 2)
+    {
+        int a = seq[i], b = seq[i + 1];
+        if (a < b)
+            std::swap(a, b);
         bigs.push_back(a);
         smalls.push_back(b);
     }
@@ -90,9 +105,11 @@ void PmergeMe::fordJohnsonSort(std::vector<int> &seq) {
     fordJohnsonSort(bigs);
 
     std::vector<size_t> order = generateJacobsthalOrder(smalls.size());
-    for (size_t i = 0; i < order.size(); ++i) {
+    for (size_t i = 0; i < order.size(); ++i)
+    {
         size_t idx = order[i];
-        if (idx < smalls.size()) {
+        if (idx < smalls.size())
+        {
             int v = smalls[idx];
             std::vector<int>::iterator it = std::upper_bound(bigs.begin(), bigs.end(), v);
             bigs.insert(it, v);
@@ -101,14 +118,18 @@ void PmergeMe::fordJohnsonSort(std::vector<int> &seq) {
     seq = bigs;
 }
 
-void PmergeMe::fordJohnsonSort(std::deque<int> &seq) {
+void PmergeMe::fordJohnsonSort(std::deque<int> &seq)
+{
     size_t n = seq.size();
-    if (n <= 1) return;
+    if (n <= 1)
+        return;
 
     std::deque<int> bigs, smalls;
-    for (size_t i = 0; i + 1 < n; i += 2) {
-        int a = seq[i], b = seq[i+1];
-        if (a < b) std::swap(a, b);
+    for (size_t i = 0; i + 1 < n; i += 2)
+    {
+        int a = seq[i], b = seq[i + 1];
+        if (a < b)
+            std::swap(a, b);
         bigs.push_back(a);
         smalls.push_back(b);
     }
@@ -118,9 +139,11 @@ void PmergeMe::fordJohnsonSort(std::deque<int> &seq) {
     fordJohnsonSort(bigs);
 
     std::vector<size_t> order = generateJacobsthalOrder(smalls.size());
-    for (size_t i = 0; i < order.size(); ++i) {
+    for (size_t i = 0; i < order.size(); ++i)
+    {
         size_t idx = order[i];
-        if (idx < smalls.size()) {
+        if (idx < smalls.size())
+        {
             int v = smalls[idx];
             std::deque<int>::iterator it = std::upper_bound(bigs.begin(), bigs.end(), v);
             bigs.insert(it, v);
@@ -129,7 +152,8 @@ void PmergeMe::fordJohnsonSort(std::deque<int> &seq) {
     seq = bigs;
 }
 
-void PmergeMe::sortAndMeasure(void) {
+void PmergeMe::sortAndMeasure(void)
+{
     std::vector<int> vCopy = _vector;
     std::deque<int> dCopy = _deque;
 
